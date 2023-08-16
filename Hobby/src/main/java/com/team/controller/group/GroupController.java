7546ce56fe5e338015a0e5ee22e70208adfe39c1
shinsil0601 <tlstlf36@naver.com>
2023-08-16 20:49:08 +0900

    모임초안02

--- Hobby/src/main/java/com/team/controller/group/GroupController.java
+++ Hobby/src/main/java/com/team/controller/group/GroupController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.team.group.service.GroupService;
+import com.team.group.vo.GroupVO;
 
 @Controller
 public class GroupController {
@@ -16,16 +17,16 @@ public class GroupController {
 	private GroupService groupService;
 	
 	@GetMapping("/groupList.do")
-    public ModelAndView groupListForm() {
+    public ModelAndView getAllGroups() {
 		ModelAndView mv = new ModelAndView("group/groupList");
-		List<String[]> posts = new ArrayList<>();
-		posts.add(new String[] {"제목 1", "내용 1", "thumbnail1.jpg"});
-		posts.add(new String[] {"제목 2", "내용 2", "thumbnail2.jpg"});
-		mv.addObject("posts", posts);
+		List<GroupVO> glist = groupService.getAllGroups();
+		mv.addObject("glist", glist);
         return mv;
     }
-
-   
+	@GetMapping("/group_writeForm.do")
+	public ModelAndView getGroupWriteForm() {
+		return new ModelAndView("group/groupWrite");
+	}
     
 	
 	
