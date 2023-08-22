2a3fb5dc8d0d95f5718f1f9301cfee782a98aa90
dlthdod <sdus7880@gmail.com>
2023-08-22 15:58:19 +0900

    mypage

--- Hobby/src/main/java/com/team/myreview/dao/MyReviewDAO.java
+++ Hobby/src/main/java/com/team/myreview/dao/MyReviewDAO.java
@@ -1,5 +1,18 @@
 package com.team.myreview.dao;
 
-public class MyReviewDAO {
+import java.util.List;
 
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.team.myreview.vo.MyReviewVO;
+
+@Repository
+public class MyReviewDAO {
+	@Autowired
+	private SqlSessionTemplate sessionTemplate;
+	public List<MyReviewVO> getMyReivew(){
+		return sessionTemplate.selectList("mypage.myreview");
+	}
 }
